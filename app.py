f78fecf199b42e4e3fadaea47bc66c7748f4cc51
Mahendra Kumar <Mahendra.Kumar4@unilever.com>
2023-07-27 13:16:34 +0530

    add the updated code into main branch

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import sklearn
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import xgboost
 import model_history
 from model_history import ModelVersioning
@@ -14,27 +15,39 @@ X = data[['daily_time_spent_on_site', 'age' , 'area_income' ,'daily_internet_usa
 y=data['clicked_on_ad']
 
 
+reg = LinearRegression().fit(X, y)
+reg.score(X, y)
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
+
 
 X, y = load_iris(return_X_y=True)
 clf = LogisticRegression(random_state=0).fit(X, y)
 
+
+
 model = xgboost.XGBClassifier().fit(X_train, y_train)
 clf.predict(X[:2, :])
 predict = model.predict(X_test)
-
-m1 = ModelVersioning(model)
-df_model = m1.Model_Artifacts()
-print(df_model.head(10))
-
-
 
 X, y = load_iris(return_X_y=True)
 clf = LogisticRegression(random_state=0).fit(X, y)
 
 
 clf.predict_proba(X[:2, :])
+
+m1 = ModelVersioning(model)
+df_model = m1.Model_Artifacts()
+print(df_model.head(10))
+
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression().fit(X, y)
+reg.score(X, y)
+
+
+
 
 
 
--- model_history.py
+++ model_history.py
@@ -1,6 +1,7 @@
 import pandas as pd
 #import json
 import datetime
+# this piece of code will add the mode artiact had model history into a data frame 
 class ModelVersioning:
     def __init__(self, trained_model):
         self.trained_model = trained_model
@@ -10,7 +11,8 @@ class ModelVersioning:
         df_param = pd.DataFrame([self.trained_model.get_params()]).T.reset_index().rename(
             columns={'index': 'Hyper_Parameter_name', 0: 'Hyper_Parameter_value'})
         return df_param
-
+# add comment lines to change in the code 
+# this will add the model artifacts into the model hyper parameters 
     def Model_Artifacts(self):
         df_params = self.Model_HyperParameters()
         df_params['run_time'] = datetime.datetime.now()
